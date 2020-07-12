1e77d1b2fc3d31f6146987031b1ef8c156f14750
Duc <vanduc2514@gmail.com>
2020-07-12 20:39:12 +0700

    Enable CrossOrigin from all origins

--- src/main/java/com/codegym/pookpasshow/api/TodoAPI.java
+++ src/main/java/com/codegym/pookpasshow/api/TodoAPI.java
@@ -4,16 +4,14 @@ import com.codegym.pookpasshow.model.Todo;
 import com.codegym.pookpasshow.services.todo.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("todos")
+@CrossOrigin("*")
 public class TodoAPI {
     private TodoService todoService;
 
