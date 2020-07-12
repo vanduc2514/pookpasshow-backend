17296880e4ab39e1681a451efd4817b982fafbb1
vanduc2514 <vanduc2514@gmail.com>
2020-07-12 18:17:53 +0700

    Init Project

---
+++ src/main/java/com/codegym/pookpasshow/PookpasshowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.pookpasshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PookpasshowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PookpasshowBackendApplication.class, args);
+    }
+
+}
