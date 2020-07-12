ad90d028bee264ad2d451cc3d385d1643d213517
Duc <vanduc2514@gmail.com>
2020-07-12 18:59:48 +0700

    Configure Project

---
+++ src/main/java/com/codegym/pookpasshow/model/Todo.java
@@ -0,0 +1,26 @@
+package com.codegym.pookpasshow.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todo", schema = "pookpasshow")
+@Data
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Basic
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Basic
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+}
