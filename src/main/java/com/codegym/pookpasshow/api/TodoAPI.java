c14d6b12abd3711b0b9cdc925796edefe3c7ff8a
Duc <vanduc2514@gmail.com>
2020-07-17 16:56:24 +0700

    Pass Test #1

---
+++ src/main/java/com/codegym/pookpasshow/api/TodoAPI.java
@@ -0,0 +1,29 @@
+package com.codegym.pookpasshow.api;
+
+import com.codegym.pookpasshow.model.Todo;
+import com.codegym.pookpasshow.services.todo.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoAPI {
+    private TodoService todoService;
+
+    @Autowired
+    public void setTodoService(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Todo> getAllTodo() {
+        return todoService.getAll();
+    }
+}
